19c16aae118dff13000911c926aec0785813f805
Peter Son <peteson.me@gmail.com>
2021-02-02 23:53:49 +1300

    first commit

---
+++ components/theme.js
@@ -0,0 +1,21 @@
+import { createMuiTheme } from "@material-ui/core/styles";
+import { red } from "@material-ui/core/colors";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#283E4A",
+    },
+    secondary: {
+      main: "#19857b",
+    },
+    error: {
+      main: red.A400,
+    },
+    background: {
+      default: "#F5F5F5",
+    },
+  },
+});
+
+export default theme;
